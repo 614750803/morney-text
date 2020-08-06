0ef2fdec7c8376a6a8ad885b93e7cd37a625d4c9
Songsong <614750803@qq.com>
2020-08-06 21:16:45 +0800

    模块化：Money.vue 分成4个小组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="请在这里输入内容">
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Notes"
+}
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  background: #f5f5f5;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+
+</style>
