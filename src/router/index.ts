f616fa5cb427db86e8699718f2e0ffcdd12a54d5
Songsong <614750803@qq.com>
2020-08-09 17:01:36 +0800

    将tag的string改为id和name

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,29 +4,35 @@ import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
+import EditLabel from '@/views/EditLabel.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
     {
-      path : '/' ,
-      redirect : '/money'
+      path: '/',
+      redirect: '/money'
     },
     {
-      path : '/money',
-      component : Money
+      path: '/money',
+      component: Money
     },
     {
-      path : '/labels',
-      component : Labels
+      path: '/labels',
+      component: Labels
     },
-  {
-      path : '/statistics',
-      component : Statistics
-    },{
-  path : '/*/',
-    component: NotFound
-  }
+    {
+      path: '/statistics',
+      component: Statistics
+    },
+    {
+      path: '/labels/edit',
+      component: EditLabel
+    },
+    {
+      path: '/*/',
+      component: NotFound
+    }
   ]
 ;
 
