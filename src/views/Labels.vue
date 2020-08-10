068fc307e00c3e259c87a7f0cc24c118d6f66176
Songsong <614750803@qq.com>
2020-08-10 15:04:33 +0800

    封装tagList的增删改查

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,7 +17,6 @@
 
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import tagListModel from '@/models/tagListModel';
 
 @Component
 export default class Labels extends Vue {
