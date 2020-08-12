19bfa4f292667792510fd5d7a3e9ffa4fcb4f2f3
Songsong <614750803@qq.com>
2020-08-12 14:36:30 +0800

    完成列表的css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,21 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />
-    <div>
-      type:{{type}}
-      <br/>
-      interval:{{interval}}
-    </div>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record">
+
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -22,6 +31,30 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs},
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    type HashTableValue = { title: string; items: RecordList[] }
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
@@ -30,6 +63,29 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  @extend %item;
+  background: white;
+}
+
+.notes {
+  margin-right: auto;
+  margin-left: 18px;
+  color: #999;
+}
+
 ::v-deep .type-tabs-item {
   background: white;
 
@@ -41,6 +97,7 @@ export default class Statistics extends Vue {
     }
   }
 }
+
 ::v-deep .interval-tabs-item {
   height: 48px;
 }
