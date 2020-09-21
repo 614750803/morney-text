27c69bfd4eeafb31bb0bfa718120672c72728bd9
Songsong <614750803@qq.com>
2020-09-21 18:55:40 +0800

    完善图表细节

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,9 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Chart :options="x"/>
+    <div class="chart-wrapper" ref="chartWrapper">
+      <Chart class="chart" :options="x"/>
+    </div>
     <ol v-if="groupedList.length>0">
       <li v-for="(group,index) in groupedList" :key="index">
         <h3 class="title">{{ beautiful(group.title) }} <span>￥{{group.total}}</span></h3>
@@ -40,7 +42,9 @@ export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
   }
-
+  mounted() {
+    (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
+  }
   beautiful(string: string) {
     const day = dayjs(string);
     const now = dayjs();
@@ -59,18 +63,29 @@ export default class Statistics extends Vue {
   }
   get x() {
     return {
+      grid: {
+        left: 0,
+        right: 0,
+      },
       xAxis: {
         type: 'category',
         data: [
           '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
           '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
           '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-        ]
-      },
+        ],
+      axisTick: {alignWithLabel: true},
+      axisLine: {lineStyle: {color: '#666'}}
+    },
       yAxis: {
-        type: 'value'
+        type: 'value',
+        show:false
       },
       series: [{
+        symbol: 'circle',
+        symbolSize: 12,
+        itemStyle: {borderWidth: 1, color: '#666', borderColor: '#666'},
+        // lineStyle: {width: 10},
         data: [
           820, 932, 901, 934, 1290, 1330, 1320,
           820, 932, 901, 934, 1290, 1330, 1320,
@@ -79,7 +94,11 @@ export default class Statistics extends Vue {
         ],
         type: 'bar'
       }],
-      tooltip: {show: true}
+      tooltip: {
+        show: true, triggerOn: 'click',
+        position: 'top',
+        formatter: '{c}'
+      }
     };
   }
   get recordList() {
@@ -162,5 +181,15 @@ export default class Statistics extends Vue {
 
 ::v-deep .interval-tabs-item {
   height: 48px;
+}
+.chart {
+  width: 430%;
+  height: 40vh;
+  &-wrapper {
+    overflow: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
 }
 </style>
